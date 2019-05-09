45762105aad5a2998e33ce5b2dd787d16890ec76
Marcio Silva <marciotexas@yahoo.com>
2019-05-09 17:06:17 -0500

    initial

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-let expenses = [1, 3, 6]
-
-let total = 0
-
-
-
-
-function amountTotal(amount) {
-  
-   for (let i = 0; i < amount.length; ++i) {
-        total += amount[i]; // add each element in an array to total
-   }
-   return total;// return sum of elements in array
-}
-
-document.getElementById("add").addEventListener("click", amountTotal);
-document.getElementById("result").innerHTML= "$" + total;
-
-
+document.getElementById('add').addEventListener('click', function() {
+    let inputText = document.getElementsByTagName('input')[0].value
+    let li = document.createElement('li');
+    li.innerText = inputText;
+    document.querySelector('ul').append(li);
+})
